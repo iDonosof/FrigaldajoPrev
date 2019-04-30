f58d2434d1cad40ca8352f590a17bb5dbb8bf23c
Aldo Mendez <aldo.mendez@alumnos.duoc.cl>
2019-04-29 23:45:52 -0400

    Estas son las clases que definimos las clases anteriores, contienen

---
+++ Frigaldajo.Negocios/Alumnos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frigaldajo.Negocios
+{
+    public class Alumnos
+    {
+        #region Campos
+        private string _rut;
+        private string _nombre;
+        private string _apellido;
+        private long _telefono;
+        private string _correo;
+        private int _edad;
+        private string _jornada; 
+        #endregion
+
+        #region Propiedades
+        public string Rut
+        {
+            get { return _rut; }
+            set { _rut = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+
+        public long Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+
+        public string Correo
+        {
+            get { return _correo; }
+            set { _correo = value; }
+        }
+
+        public int Edad
+        {
+            get { return _edad; }
+            set { _edad = value; }
+        }
+
+        public string Jornada
+        {
+            get { return _jornada; }
+            set { _jornada = value; }
+        } 
+        #endregion
+
+        #region Contructor E Inicializador
+        public Alumnos()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            Rut = string.Empty;
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            Telefono = 0;
+            Correo = string.Empty;
+            Edad = 0;
+            Jornada = string.Empty;
+        }
+        #endregion
+
+        #region Metodos
+
+        #endregion
+    }
+}
---
+++ Frigaldajo.Negocios/Externo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frigaldajo.Negocios
+{
+    public class Externo
+    {
+        #region Campos
+        private string _rut;
+        private string _nombre;
+        private long _telefono;
+        private string _correo;
+        private int _edad; 
+        #endregion
+
+        #region Propiedades
+        public string Rut
+        {
+            get { return _rut; }
+            set { _rut = value; }
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public long Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+        public string Correo
+        {
+            get { return _correo; }
+            set { _correo = value; }
+        }
+
+        public int Edad
+        {
+            get { return _edad; }
+            set { _edad = value; }
+        } 
+        #endregion
+
+        #region Constructor e Inicializador
+        public Externo()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            Rut = string.Empty;
+            Nombre = string.Empty;
+            Telefono = 0;
+            Correo = string.Empty;
+            Edad = 0;
+        } 
+        #endregion
+
+        #region Metodos
+
+        #endregion
+    }
+}
