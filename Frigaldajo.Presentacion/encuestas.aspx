04e597b24a663718fc284f2d866b5217bb3c546f
Joaquin Marquez <j.marquez.p@outlook.es>
2019-05-05 18:44:27 -0400

    Encuesta

---
+++ Frigaldajo.Presentacion/encuestas.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="encuestas.aspx.cs" Inherits="Frigaldajo.Presentacion.encuestas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="cajaEncuesta">
+        <h1 class="text-center orange-title">Encuentas</h1>
+        <br />
+        <div id="centrarCaja">
+            <table>
+            <tr> <!--Primera Fila-->
+                <td>
+                    
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/triste.png" />
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/neutral.png" />
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/sonriente.png" />
+                </td>
+            </tr>
+            <tr>
+                <td class="margin-text">
+                    <p class="orange-title text-right">¿Te Gusto el Evento?</p>
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbEvento1" GroupName="evento" runat="server" />
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbEvento2" GroupName="evento" runat="server" />
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbEvento3" GroupName="evento" runat="server" />
+                </td>
+            </tr>
+            <tr> <!--Segunda Fila-->
+                <td>
+                    
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/triste.png" />
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/neutral.png" />
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/sonriente.png" />
+                </td>
+            </tr>
+            <tr>
+                <td class="margin-text">
+                    <p class="orange-title text-right">¿Como estuvo la organización?</p>
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbOrganizacion1" GroupName="organizacion" runat="server" />
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbOrganizacion2" GroupName="organizacion" runat="server" />
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbOrganizacion3" GroupName="organizacion" runat="server" />
+                </td>
+            </tr>
+            <tr> <!--Tercera Fila-->
+                <td>
+                    
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/triste.png" />
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/neutral.png" />
+                </td>
+                <td class="margin-text">
+                    <img src="IMG/sonriente.png" />
+                </td>
+            </tr>
+            <tr>
+                <td class="margin-text">
+                    <p class="orange-title text-right">¿Que te parecio el tiempo de espera?</p>
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbTiempo1" GroupName="tiempo" runat="server" />
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbTiempo2" GroupName="tiempo" runat="server" />
+                </td>
+                <td class="margin-text">
+                    <asp:RadioButton class="radio" ID="rdbTiempo3" GroupName="tiempo" runat="server" />
+                </td>
+            </tr>
+        </table>
+        <br />
+        <table> <!--Seccion final-->
+            <tr>
+                <td>
+                    <p class="orange-title">¿Alguna sugerencia, queja o comentario que quieras hacer?</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox class="textboxEncuesta" ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button class="botonEnviar" ID="btnAgregar_Externos" runat="server" Text="Enviar" />
+                </td>
+            </tr>
+        </table>
+        </div>
+    </div>
+</asp:Content>
---
+++ Frigaldajo.Presentacion/encuestas.aspx.designer.cs
@@ -0,0 +1,114 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Frigaldajo.Presentacion {
+    
+    
+    public partial class encuestas {
+        
+        /// <summary>
+        /// Control rdbEvento1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbEvento1;
+        
+        /// <summary>
+        /// Control rdbEvento2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbEvento2;
+        
+        /// <summary>
+        /// Control rdbEvento3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbEvento3;
+        
+        /// <summary>
+        /// Control rdbOrganizacion1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbOrganizacion1;
+        
+        /// <summary>
+        /// Control rdbOrganizacion2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbOrganizacion2;
+        
+        /// <summary>
+        /// Control rdbOrganizacion3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbOrganizacion3;
+        
+        /// <summary>
+        /// Control rdbTiempo1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbTiempo1;
+        
+        /// <summary>
+        /// Control rdbTiempo2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbTiempo2;
+        
+        /// <summary>
+        /// Control rdbTiempo3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rdbTiempo3;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control btnAgregar_Externos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregar_Externos;
+    }
+}
